5fc808c36c8eb7a553086da86a60b68e7b6c5a7f
Sunandan Ghimire <ghimiresunandan@gmail.com>
2022-08-09 22:12:02 +0545

    Booking Controller Update

--- src/main/java/com/example/demo/Controller/BookingController.java
+++ src/main/java/com/example/demo/Controller/BookingController.java
@@ -14,26 +14,31 @@ public class BookingController {
     @Autowired
     BookingService bookingService;
 
-//    @PostMapping("/addBooking")
-//    public Booking saveBooking(@RequestBody Booking booking){
-//        return bookingService.saveBooking(booking);
-//    }
-//
-//    @GetMapping("/getBooking/{id}")
-//    public Booking getBooking(@PathVariable int id){
-//        return bookingService.getById(id);
-//    }
-//
-//    @GetMapping("/getBookings")
-//    public List<Booking> getBookings(){
-//        return bookingService.getAllBookings();
-//    }
-//
-//
-//    @PutMapping("/updateBooking/{id}")
-//    public Booking updateBooking(@RequestBody Booking booking){
-//        return bookingService.updateBooking(booking);
-//    }
+    @PostMapping("/addBooking")
+  public Booking saveBooking(@RequestBody Booking booking){
+        return bookingService.saveBooking(booking);
+    }
+
+    @GetMapping("/getBooking/{id}")
+    public Booking getBooking(@PathVariable int id){
+        return bookingService.getById(id);
+    }
+
+    @GetMapping("/getBookings")
+    public List<Booking> getBookings(){
+        return bookingService.getAllBookings();
+    }
+
+
+    @PutMapping("/updateBooking/{id}")
+    public Booking updateBooking(@RequestBody Booking booking){
+        return bookingService.updateBooking(booking);
+    }
+
+    @DeleteMapping("deleteBooking/{id}")
+    public Booking deleteBooking(@PathVariable int id){
+        return bookingService.deleteBooking(id);
+    }
 
 
 
